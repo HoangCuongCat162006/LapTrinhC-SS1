68e0aaf36c199bf46fe2289ede304a90f7906121
HoangCuongCat162006 <Cathoang162006@gmail.com>
2024-11-20 23:45:56 +0700

    Upload btvn 2

---
+++ btvn 2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	// khai bao bien va khoi tao hai so nguyen 
+	int num1 = 10; //so nguyen thu nhat
+	int num2 = 9; //so nguyen thu hai 
+	
+	// tinh tong hai so nguyen va luu ket qua vao bien sum 
+	int sum = num1 + num2; 
+	
+	// in ket qua ra man hinh 	
+    printf (" tong cua %d va %d la : %d\n" , num1, num2, sum);
+    
+    return 0;
+}
